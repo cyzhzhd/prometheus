b20c765da92f9b44c467289c81a2b8181d013734
cyzhzhd <cyzhzhd@gmail.com>
2022-04-03 22:08:18 +0900

    api에 Gauge 추가

--- helloWorld.py
+++ helloWorld.py
@@ -1,21 +1,34 @@
 import http.server
 import random
-from prometheus_client import start_http_server, Counter
+from prometheus_client import start_http_server, Counter, Gauge
 
 REQUESTS = Counter('hello_worlds_total', 'Hello Worlds requested.')
 EXCEPTIONS = Counter('hello_worlds_exceptions_total',
                      'Exceptions serving Hello World.')
+INPROGRESS = Gauge('hello_worlds_inprogress',
+                   'Number of Heelo Worlds in progress')
+LAST = Gauge('hello_worlds_last_time_seconds',
+             'The last time a Hello World was served.')
 
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
     @EXCEPTIONS.count_exceptions()
+    # @INPROGRESS.track_inprogress()
     def do_GET(self):
         REQUESTS.inc()
+        INPROGRESS.inc()
+
         if random.random() < 0.2:
+            LAST.set_to_current_time()
+            INPROGRESS.dec()
             raise Exception
+
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b"Hello World")
+
+        LAST.set_to_current_time()
+        INPROGRESS.dec()
 
 
 if __name__ == "__main__":
